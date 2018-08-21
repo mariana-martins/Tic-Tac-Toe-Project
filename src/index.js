14fc898707140101bc410a67861de5b7c7d70045
mariana-martins <mariana_martins@id.uff.br>
2018-08-21 14:31:51 +1200

    Adding turns for the game modifying the state

--- src/index.js
+++ src/index.js
@@ -2,28 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
-    handleClick(i) {
-      const squares = { ...this.state.squares};
-      squares[i] = 'X';
-      this.setState({squares});
+  handleClick(i) {
+    // Take a copy of the existing state
+    const squares = { ...this.state.squares };
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    // Set the new squares object to state
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
 
   renderSquare = i => (
@@ -31,7 +35,7 @@ class Board extends React.Component {
   );
 
   render() {
-    const status = "Next Player: x";
+    const status = "Next Player: " + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
